dafdb3ed0986da4724e3c86e0a792db9b65728b1
BimxyzDev <bimaputra436123@gmail.com>
2026-03-24 21:38:52 +0700

    Create setting.js

---
+++ api/setting.js
@@ -0,0 +1,4 @@
+// api/setting.js
+const API_KEY = "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";
+const API_URL = "https://api.groq.com/openai/v1/chat/completions";
+const MODEL = "llama-3.3-70b-versatile";
