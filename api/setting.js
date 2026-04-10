3a5281e9820f324ba7f749fbaefe42996555364e
BimxyzDev <bimaputra436123@gmail.com>
2026-04-10 18:51:42 +0700

    Update setting.js

--- api/setting.js
+++ api/setting.js
@@ -1,4 +1,9 @@
 // api/setting.js
-const API_KEY = "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";
+const API_KEY = "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";
 const API_URL = "https://api.groq.com/openai/v1/chat/completions";
 const MODEL = "llama-3.3-70b-versatile";
+
+// Export ke window
+window.API_KEY = API_KEY;
+window.API_URL = API_URL;
+window.MODEL = MODEL;
