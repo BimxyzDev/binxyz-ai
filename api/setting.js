c3cb0f5d42b3ce3802c77c1f9d19cde2ce2cb168
BimxyzDev <bimaputra436123@gmail.com>
2026-04-20 13:40:06 +0700

    Delete api/setting.js

--- api/setting.js
+++
@@ -1,9 +0,0 @@
-// api/setting.js
-const API_KEY = "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";
-const API_URL = "https://api.groq.com/openai/v1/chat/completions";
-const MODEL = "llama-3.3-70b-versatile";
-
-// Export ke window
-window.API_KEY = API_KEY;
-window.API_URL = API_URL;
-window.MODEL = MODEL;
